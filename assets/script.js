b555396a52e4cbeb48aa14e64e5e37385bc95a87
Eduardo <sapienciando@gmail.com>
2023-05-06 12:38:01 -0400

    novas pages

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
-function openPopup() {
+function openAbacaxi() {
     var leftPos = (screen.width - 400) / 2;
     var topPos = (screen.height - 600) / 2;
-    var popup = window.open('popup.html', 'popup', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=600, top=' + topPos + ', left=' + leftPos);
+    var popup = window.open('abacaxi.html', 'popup', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=600, top=' + topPos + ', left=' + leftPos);
   }
   
+  function openMaracuja() {
+    var leftPos = (screen.width - 400) / 2;
+    var topPos = (screen.height - 600) / 2;
+    var popup = window.open('maracuja.html', 'popup', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=600, top=' + topPos + ', left=' + leftPos);
+  }
+  function openMorango() {
+    var leftPos = (screen.width - 400) / 2;
+    var topPos = (screen.height - 600) / 2;
+    var popup = window.open('morango.html', 'popup', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=600, top=' + topPos + ', left=' + leftPos);
+  }
