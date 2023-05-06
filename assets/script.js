94e05a7723cd054cf3a9e9a8d5108bba026921ae
Eduardo <sapienciando@gmail.com>
2023-05-06 12:20:12 -0400

    btoes

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+function openPopup() {
+    var leftPos = (screen.width - 400) / 2;
+    var topPos = (screen.height - 600) / 2;
+    var popup = window.open('popup.html', 'popup', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=600, top=' + topPos + ', left=' + leftPos);
+  }
+  
