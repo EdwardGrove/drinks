1ef73cc3894e8cba4396a954b0af8c78bf54149a
Eduardo <sapienciando@gmail.com>
2023-05-08 08:02:44 -0400

    qewwq

--- assets/script.js
+++ assets/script.js
@@ -14,3 +14,12 @@ function openAbacaxi() {
     var topPos = (screen.height - 600) / 2;
     var popup = window.open('morango.html', 'popup', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=600, top=' + topPos + ', left=' + leftPos);
   }
+  // Seleciona o botão e o cartão
+const cardButton = document.querySelector(".card-button");
+const card = document.querySelector(".card");
+
+// Adiciona um ouvinte de evento de clique ao botão
+cardButton.addEventListener("click", function() {
+  // Torna o cartão visível
+  card.style.display = "block";
+});
